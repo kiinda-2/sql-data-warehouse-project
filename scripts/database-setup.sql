8e443c9e5f2b94383839cf80030de7d854ae3a77
Nyawira Okinda <125373629+kiinda-2@users.noreply.github.com>
2025-03-03 18:43:45 +0300

    Update and rename placeholder to database-setup.sql

---
+++ scripts/database-setup.sql
@@ -0,0 +1,14 @@
+--Create database
+CREATE DATABASE DataWarehouseProject
+
+--Use db
+USE DataWarehouseProject
+
+ --Create schemas 
+CREATE SCHEMA bronze
+  
+CREATE SCHEMA silver
+GO
+CREATE SCHEMA gold
+
+
